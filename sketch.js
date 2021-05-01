ceff2eaddcf484118a823b8a7d08f58bc65d04d3
Ganesh336 <77608888+Ganesh336@users.noreply.github.com>
2021-05-01 15:16:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,19 @@
 var background
-var tom;
-var jerry;
+var cat,catImg;
+var mouse,mouseImg;
 
 
 
 function preload() {
     //load the images here
+    catImg = loadAnimation("cat1.png","cat2.png","cat3.png","cat4.png");
+    mouseImg = loadAnimation("mouse1.png","mouse2.png","mouse3.png","mouse4.png");
 }
 
 function setup(){
     createCanvas(1000,800);
     //create tom and jerry sprites here
+    cat = createSprite(800,100,50,50)
 
 }
 
